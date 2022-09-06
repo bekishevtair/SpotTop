3f31d44e56a302afc77ec4ace57239c88055dcdc
Tair Bekishevm <bekishevtair@gmail..com>
2022-09-07 00:05:26 +0200

    added crypto widget

--- src/assets/scripts/int/calc.js
+++ src/assets/scripts/int/calc.js
@@ -19,3 +19,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
         return false;
     };
 });
+
+
+// WIDGETS
+var btc = document.getElementById("bitcoin"),
+    ltc = document.getElementById("litecoin"),
+    eth = document.getElementById("ethereum"),
+    doge = document.getElementById("dogecoin"),
+    usdt = document.getElementById("tether")
+
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Clitecoin%2Cethereum%2Cdogecoin%2Ccardano%2Csolana%2Cripple&vs_currencies=usd",
+    "method": "GET",
+    "headers": {}
+}
+
+$.ajax(liveprice).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    ltc.innerHTML = response.litecoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+    usdt.innerHTML = response.tether.usd;
+
+});
